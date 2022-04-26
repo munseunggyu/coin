2fb9d62904fc0973371998de60f2be1e2b90b2ef
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-04-27 05:05:37 +0900

    Add Coin template and price,chart Page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import Coins from './routes/Coins';
 import Coin from './routes/Coin';
+import Price from './routes/Price';
+import Chart from './routes/Chart';
 function Router(){
   return(
     <BrowserRouter>
       <Routes>
-        <Route  path="/:coinId" element={<Coin />} />
+        <Route  path="/:coinId/*" element={<Coin />} >
+        <Route path='price' element={<Price />} />
+        <Route path='chart' element={<Chart />} />
+        </Route>
         <Route path='/' element={<Coins />} /> 
       </Routes>
     </BrowserRouter>
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import {useParams,useLocation} from 'react-router-dom';
+import {useParams,useLocation,Outlet} from 'react-router-dom';
 import styled from 'styled-components';
+
 
 const Container = styled.div`
   padding:0 20px;
@@ -12,43 +13,151 @@ const Header = styled.header`
   display:flex;
   justify-content:center;
   align-items:center;
+  margin-bottom:30px;
 `
 const Title = styled.h1`
   color:${props => props.theme.accentColor};
+  font-size:50px;
 `
 const Loader = styled.div`
   text-align:center;
 `
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
 interface ILocation {
   state:{
   name:string;
   };
 }
+interface IInfo {
+  id :string;
+  name :string;
+  symbol :string;
+  rank :number;
+  is_new :boolean;
+  is_active :boolean;
+  type :string;
+  description :string;
+  message :string;
+  open_source :boolean;
+  started_at :string;
+  development_status :string;
+  hardware_wallet :boolean;
+  proof_type :string;
+  org_structure :string;
+  hash_algorithm :string;
+  first_data_at :string;
+  last_data_at :string;
+}
+
+interface IPrice{
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD:{
+      ath_date: string;
+      ath_price:number;
+      market_cap:number;
+      percent_change_1h:number;
+      percent_change_1y:number;
+      percent_change_6h:number;
+      percent_change_7d:number;
+      percent_change_12h:number;
+      percent_change_15m:number;
+      percent_change_24h:number;
+      percent_change_30d:number;
+      percent_change_30m:number;
+      percent_from_price_ath:number;
+      price:number;
+      volume_24h:number;
+      volume_24h_change_24h:number;
+    }
+  };
+}
 
 
 function Coin(){
   const {coinId} = useParams();
-  const [loding,setLoding] = useState(true)
+  const [loading,setLoding] = useState(true)
   const {state} = useLocation() as ILocation;
-  const [info,setInfo] = useState({})
+  const [info,setInfo] = useState<IInfo>({} as any)
+  const [priceInfo,setPriceInfo] = useState<IPrice>({} as any)
   useEffect(() => {
     (async () => {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
         ).json();
-      const priceData = await (
+        setInfo(infoData)
+        const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-        setInfo(infoData)
+        setPriceInfo(priceData)
+        setLoding(false)
     })()
-  },[])
+  },[coinId])
   return (
     <Container>
       <Header>
-        <Title> {state?.name || "Go homePage" } </Title>
+        <Title> {state?.name ? state.name : loading ? "Loading..." : info.name} </Title>
       </Header>
-      {loding ? <Loader>Loading...</Loader> : null}
+      {loading ? <Loader>Loading...</Loader> : (
+        <>
+        <Overview>
+          <OverviewItem>
+            <span>Rank:</span>
+            <span>{info?.rank}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Symbol:</span>
+            <span>${info?.symbol}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Open Source:</span>
+            <span>{info?.open_source ? "Yes" : "No"}</span>
+          </OverviewItem>
+        </Overview>
+        <Description>{info?.description}</Description>
+        <Overview>
+          <OverviewItem>
+            <span>Total Suply:</span>
+            <span>{priceInfo.total_supply}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Max Supply:</span>
+            <span>{priceInfo.max_supply}</span>
+          </OverviewItem>
+        </Overview>
+        <Outlet />
+      </>
+      )}
     </Container>
   )
 }
